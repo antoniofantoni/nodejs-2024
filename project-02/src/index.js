54b64a053aa3768471bd3932bfabd3ec6376cf18
Antonio Fantoni <antonio.b.fantoni@gmail.com>
2024-10-07 20:36:18 -0300

    primeiro commit

--- project-02/src/index.js
+++ project-02/src/index.js
@@ -1,2 +1,10 @@
 const express = require("express")
 const app = express();
+
+app.get('/', (req, res) => {
+	response.send("API disponível")
+})
+
+app.listen(3000, () => {
+console.log("Servidor rodando na porta 3000")
+})
